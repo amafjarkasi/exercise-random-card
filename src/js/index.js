fc56cf4daf5a9693bd100223f7ed1a94d502deb5
amafjarkasi <amafjarkasi@gmail.com>
2020-10-06 23:18:32 +0000

    latest changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,38 @@
 /* eslint-disable */
 
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
+function randomSuitInfo() {
+  let cardSuit = [
+    "&#9824;",
+    "<font color=red>&#9829;</font>",
+    "&#9827;",
+    "&#9830;"
+  ];
+
+  let suitCalc = cardSuit[Math.floor(Math.random() * cardSuit.length)];
+
+  return suitCalc;
+}
+
+function randomNumberInfo() {
+  let cardNumbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  let cardCalc = cardNumbers[Math.floor(Math.random() * cardNumbers.length)];
+
+  return cardCalc;
+}
+
+function buildCardInfo() {
+  let suitInfo = randomSuitInfo();
+
+  document.getElementById("suitTop").innerHTML = suitInfo;
+  document.getElementById("suitBottom").innerHTML = suitInfo;
+
+  document.getElementById("numbersCard").innerHTML = randomNumberInfo();
+}
+
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  buildCardInfo();
 };
